6d966a2393a106c0fe0e0777c87b4cfb0d9d3029
flyingroc <haipper@qq.com>
2017-05-18 16:47:05 +0800

    log4ntest

---
+++ log4nettest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Common;
+
+namespace log4nettest
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.ThreadException += Application_ThreadException;
+            //Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            //AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new log4Ntest());
+        }
+
+        /// <summary>
+        /// 绑定程序中的异常处理
+        /// </summary>
+        private static void BindExceptionHandler()
+        {
+            //设置应用程序处理异常方式：ThreadException处理
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            //处理UI线程异常
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            //处理未捕获的异常
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+        /// <summary>
+        /// 处理UI线程异常
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            LogHelper.WriteLog(null, e.Exception as Exception);
+        }
+        /// <summary>
+        /// 处理未捕获的异常
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogHelper.WriteLog(null, e.ExceptionObject as Exception);
+        }
+    }
+}
---
+++ log4nettest/log4Ntest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Common;
+
+namespace log4nettest
+{
+    public partial class log4Ntest : Form
+    {
+        public log4Ntest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            log4net.ILog log = log4net.LogManager.GetLogger("MyLogger");
+            if (log.IsDebugEnabled)
+            {
+                log.Debug("hello");
+            }
+            */
+            LogHelper.WriteLog(string.Format("当前的时间{0}", DateTime.Now.ToString()));
+        }
+    }
+}
